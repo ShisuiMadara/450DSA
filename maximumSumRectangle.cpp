0dfd7834234eec7a08955f534546a86f53c40a7c
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-07-13 20:13:18 +0530

    progress

---
+++ maximumSumRectangle.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    
+    int kadane(vector<int>&v)
+    {
+        int ans = v[0];
+        int here = v[0];
+        
+        for(int i = 1; i < v.size(); ++i)
+        {
+            here = max(here+v[i], v[i]);
+            ans = max(ans,here);
+        }
+        
+        return ans;
+    }
+    
+     int maximumSumRectangle(int R, int C, vector<vector<int>> M) {
+       int mx=INT_MIN;
+       for(int i=0;i<R;i++){
+           vector<int>v(C,0);
+           for(int i1=i;i1<R;i1++){
+               for(int j=0;j<C;j++){
+                   v[j]+=M[i1][j];
+               }
+               mx=max(mx,kadane(v));
+           }
+       }return (mx);
+   }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>> v(N, vector<int>(M));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++) cin >> v[i][j];
+        Solution ob;
+        cout << ob.maximumSumRectangle(N, M, v) << "\n";
+    }
+}  // } Driver Code Ends
