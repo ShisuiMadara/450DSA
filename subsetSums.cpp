76ae0f030d9320f29c4e10700c4902afef0bffd6
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-07-24 23:25:54 +0530

    progress

---
+++ subsetSums.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MOD= 1000000007;
+
+
+
+void solve()
+{
+    int n, from,to;
+    cin>>n>>from>>to;
+
+    vector<int>arr(n);
+
+    for(int i = 0; i<n; ++i)
+    {
+        int x;
+        cin>>x;
+
+        arr[i] = x;
+    }
+
+    
+}
+
+
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+     ll test=1;
+//cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
