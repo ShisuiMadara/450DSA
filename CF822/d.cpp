5bfec8c52e9561a164c765f5db6f815e3b01dcab
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-09-29 21:15:09 +0530

    progress

---
+++ CF822/d.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+typedef long long ll;
+const int MOD= 1000000007;
+
+
+void solve()
+{
+    
+}
+
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+     ll test=1;
+cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
