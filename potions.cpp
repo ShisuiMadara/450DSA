394891fac636dbde7191c016020168757edc4c14
shisuimadara <2020ucs0090@iitjammu.ac.in>
2023-03-18 13:45:20 +0530

    progress

--- potions.cpp
+++ potions.cpp
@@ -1,41 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n;
-    cin>>n;
+int main(){
 
-    vector<int>arr(n);
+	int n; 
+    cin >> n;
 
-    for(auto&c:arr)cin>>c;
+	priority_queue<long long, vector<long long>, greater<long long> > pq;
 
-    vector<int>dp(n+1,0);
+	long long S = 0;
+	
+	for(int i = 1;i <= n;i++){
+		long long x; 
+        cin >> x;
 
-    for(int i = 1; i<=n; ++i) {
-        dp[i] = max(dp[i] , dp[i-1] + arr[i]);
-    }
-
-    // for(auto&c:dp)cout<<c<<" ";
-
-    // cout<<endl; 
-    int ans = 0;
-
-    int flag = 0;
-    int cnt = 0;
-
-    for(int i = 0; i <=n; ++i) {
-
-        if(dp[i] > 0) {
-            flag =1 ;
-         
-        }
-        
-        if(flag)
-            if(dp[i] > 0) {
-                cnt ++;
-            }
-        }
-
-    cout<<cnt;
-
+		S += x;
+		pq.push(x);
+		
+		while(S < 0){
+			S -= pq.top();
+            // cout<<pq.top()<<endl;
+			pq.pop();
+		}
+	}
+	long long ans = pq.size();
+	cout << ans;
 }
